573ae201ddc1c611f2026d603a14f5d2f0cf33cd
Amir <shodiyevamir999@gmail.com>
2023-02-15 17:53:19 +0500

    edded

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,6 @@ import Footer from "./components/Footer"
 function App() {
   return (
     <Routes>
-      <Route path="/footer" element={<Footer/>} />
-      <Route path="/navbar" element={<Navbar/>} />
       <Route path="/kar" element={<Karusel/>}/>
       <Route path="/" element={<Home/>} />
     </Routes>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <div>
     <Navbar />
-    
+    <Menu />
     <Footer />
     </div>
   )
