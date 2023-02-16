0b21655603ddabb23b38d9de1bf72a4bd548d7fe
Amir <shodiyevamir999@gmail.com>
2023-02-16 05:31:29 +0500

    edded

--- src/components/Karusel.jsx
+++ src/components/Karusel.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+import "../css/Carousel.css"
 
 const Karusel = () => {
+
   return (
-    <div>
-        <h1>Karusel</h1>
-        <hr />
+    <div className='a'>
+      <center>
+
+        <Carousel className='car' autoPlay={true}>
+          <div className='img1'>
+            <img className='img' src="https://xozm.ru/upload/iblock/a8e/a8e126d57ccd1a7cdee3be9680fe1b0e.png" />
+
+          </div>
+          <div className='img1'>
+            <img className='img' src="https://xozm.ru/upload/iblock/faa/faaa938059f6ee651eb72070fa7e7443.png" />
+
+          </div>
+          <div className='img1'>
+            <img className='img' src="https://xozm.ru/upload/iblock/955/95526d940427a9e1cd7fa3885657e43d.png" />
+
+          </div>
+          <div className='img1'>
+            <img className='img' src="https://xozm.ru/upload/iblock/3b6/3b6eb3879810f8931f8d8a05e1515b74.png" />
+
+          </div>
+          <div className='img1'>
+            <img className='img' src="https://xozm.ru/upload/iblock/27b/27b23bfb924e2374c6e312eedd61686d.png" />
+
+          </div>
+          <div className='img1'>
+            <img className='img' src="https://xozm.ru/upload/iblock/02d/banner_na_site_960x417_hozmarket_2.png" />
+
+          </div>
+        </Carousel>
+      </center>
+
     </div>
   )
 }
