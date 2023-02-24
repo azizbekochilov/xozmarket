38ef5b923ca381d8985ed9eadf07c6939052893d
UktamovAkbar <slavok2612@gmail.com>
2023-02-24 18:21:47 +0500

    added

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Karusel from "./components/Karusel";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
+import ProductDetail from "./pages/ProductDetail";
 function App() {
   return (
     <Routes>
-      <Route path="/kar" element={<Karusel/>}/>
-      <Route path="/" element={<Home/>} />
+      <Route path="/" element={<Home />} />
+      <Route path="/detail" element={<ProductDetail />} />
     </Routes>
-
   );
 }
 export default App;
