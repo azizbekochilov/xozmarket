8acf9caa6911bb28fb3890d725a1af7977cde33b
azizbekochilov <azizbekochilov0411@gmail.com>
2023-03-06 17:27:28 +0500

    login

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import Home from "./pages/Home";
 import Karusel from "./components/Karusel";
 import Footer from "./components/Footer"
 import About from "./pages/About";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+
 function App() {
   return (
     <Routes>
@@ -13,6 +16,9 @@ function App() {
       <Route path="/kar" element={<Karusel/>}/>
       <Route path="/" element={<Home/>} />
       <Route path="/about" element={<About/>} />
+      <Route path="/login" element={<Login/>} />
+      <Route path="/signup" element={<SignUp/>} />
+
     </Routes>
 
   );
