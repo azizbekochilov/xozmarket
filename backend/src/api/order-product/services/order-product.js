7323745bbb0cf1a93eceb7a29d24b614cd6842ec
UktamovAkbar <124454080+UktamovAkbar@users.noreply.github.com>
2023-03-15 17:44:01 +0500

    Delete backend directory

--- backend/src/api/order-product/services/order-product.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * order-product service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::order-product.order-product');
